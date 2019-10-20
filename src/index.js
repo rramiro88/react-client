4b89cb196df303f411be7093aeac5efbfb331d1e
Ramiro Alarcon <rramiro88@gmail.com>
2019-10-20 12:15:28 -0300

    Added Login redirect and Dashboard component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './Login';
+import App from './App'
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
